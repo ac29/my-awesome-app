7aff4ceeadfb62756597b7d8e51e016a676dcd75
A <alhamc@gmail.com>
2021-04-18 16:17:42 -0400

    added forecast api call

--- index.js
+++ index.js
@@ -103,6 +103,13 @@ function displayForecast() {
 
   }
 
+function getForecast(coordinates){
+  console.log(coordinates);
+  let apiKey = "7dd7f2137e6eaf2096f115b990d86b79";
+  let apiUrl= `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apikey}&units=metric`
+  axios.get(`${apiUrl}`).then(displayForecast);
+}
+
 function showTemperature(response) {
   let temperature = Math.round(response.data.main.temp);
   let windspeed = Math.round (response.data.wind.speed);
@@ -119,6 +126,9 @@ function showTemperature(response) {
   weatherDescriptionElement.innerHTML = response.data.weather[0].description;
   iconElement.setAttribute("src",`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   iconElement.setAttribute ("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coords);
+
 }
 
 function showPosition(position) {
